b406c66a4bc508229e6aead9096edf77a6dcb7d2
Savokar <66676114+Savokar@users.noreply.github.com>
2023-07-28 21:16:47 +0200

    Add files via upload

---
+++ Javascript-Website-Learning/easy/todo/app.js
@@ -0,0 +1,7 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEMS_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+function getItems() {
+                        
+}
